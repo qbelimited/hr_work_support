4d6607e293f105b14c433f232c49369121528381
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-11 20:13:54 +0000

    Update RegisterController.php
    To add the new fields added to register a user

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:150'],
+            'fname' => ['required', 'string', 'max:215'],
+            'lname' => ['required', 'string', 'max:200'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
