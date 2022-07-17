0f2da490afb91b829098d6322191d3b9fdb26e67
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-17 15:39:15 +0000

    Files update

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,32 +1,23 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+$(document).ready(function() {
+  var formFields = $('.form-field');
 
-require('./bootstrap');
+  formFields.each(function() {
+    var field = $(this);
+    var input = field.find('input');
+    var label = field.find('label');
 
-window.Vue = require('vue').default;
+    function checkInput() {
+      var valueLength = input.val().length;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+      if (valueLength > 0 ) {
+        label.addClass('freeze')
+      } else {
+            label.removeClass('freeze')
+      }
+    }
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
+    input.change(function() {
+      checkInput()
+    })
+  });
 });
